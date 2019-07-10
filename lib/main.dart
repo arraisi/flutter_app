d6e9cf9f3341ca425744337f843eba2387feba1b
Abdul Rahman Arraisi <itsarraisi@gmail.com>
2019-07-10 11:44:44 +0700

    CupertinoNavigationBar

--- lib/main.dart
+++ lib/main.dart
@@ -45,7 +45,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final GlobalKey<ScaffoldState> _scaffold = new GlobalKey<ScaffoldState>();
+  bool switchState = false;
 
   @override
   Widget build(BuildContext context) {
@@ -55,25 +55,27 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
-    return Scaffold(
-      key: _scaffold,
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-          // Center is a layout widget. It takes a single child and positions it
-          // in the middle of the parent.
+    return CupertinoPageScaffold(
+        navigationBar: CupertinoNavigationBar(
+          leading: Icon(Icons.arrow_back_ios),
+          middle: Text(
+            "Epic App",
+            style: TextStyle(color: Colors.white),
+          ),
+          trailing: Icon(Icons.account_circle),
+          backgroundColor: Colors.amber,
+        ),
+        child: SafeArea(
+            child: Container(
+                child: CupertinoSwitch(
+          value: switchState,
+          onChanged: (bool value) {
+            setState(() {
+              switchState = value;
+            });
 
-          child: CupertinoButton(
-        child: Text("Click me"),
-        onPressed: () {
-          print("You have clicked me");
-        },
-        color: Colors.red,
-        borderRadius: BorderRadius.all(Radius.circular(40)),
-      )),
-    );
+            print(value);
+          },
+        ))));
   }
 }
